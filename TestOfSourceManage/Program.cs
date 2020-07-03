260ae0e1fef1d91dee18f776ddd7cbde53457af5
xueduoyu <xueduoyu@DESKTOP-BV5MJ47>
2020-07-03 10:32:41 +0800

    第一次更改

--- TestOfSourceManage/Program.cs
+++ TestOfSourceManage/Program.cs
@@ -7,6 +7,8 @@ namespace TestOfSourceManage
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("JustTest");
+            Console.ReadKey();
         }
     }
 }
---
+++ TestOfSourceManage/Student.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestOfSourceManage
+{
+   public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
