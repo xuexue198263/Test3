766fabb8767a744176de036a81279b1991bf1e42
xueduoyu <xueduoyu@DESKTOP-BV5MJ47>
2020-07-03 08:55:29 +0800

    添加项目文件。

---
+++ TestOfSourceManage/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TestOfSourceManage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
